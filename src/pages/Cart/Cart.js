d089d8544949063d6bf8e41488467698d604f10a
Liudas Barcauskas <liudasbarcauskas@Liudass-MacBook-Pro.local>
2020-03-19 19:34:04 +0200

    added bonus task

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -5,10 +5,11 @@ import classes from './Cart.module.css'
 import { Button } from '../../components/shared/Button/Button'
 import { useSelector } from 'react-redux'
 
-// 1. TODO: Rodyti checkout tik tada kai yra karte produktu
-// 2. TODO: Sukurti forma naudojant Input.js komponenta.
+// 1. TODO: Rodyti checkout button'a tik tada kai yra karte produktu
+// 2. TODO: Checkout forma turi atsirasti tik poto, kai paspaudziame checkout.
+// 3. TODO: Sukurti forma naudojant Input.js komponenta.
 //          Formoje turi buti sie fieldai: vardas, adresas, tel nr., el. p.
-// 3. TODO: Forma turi atsirasti tik poto, kai paspaudziame checkout.
+//          Reikia pavaliduoti!!! Kas norit galit uznaudoti https://www.npmjs.com/package/yup
 // 4. TODO: Uzsakymas turi buti siunciamas i serveri ir saugomas.
 //          I uzsakyma ieina sie fieldai: suma, produktu ID's, data, ir userio ID.
 // 5. TODO: Useris taip pat keliauja i serveri. Turi buti saugomi
@@ -16,7 +17,8 @@ import { useSelector } from 'react-redux'
 // 6. TODO: Duomenys taip pat turi buti ir redux'e.
 // 7. TODO: Atvaizduoti orderius "Orders" page lenteles pavidalu.
 //          Turi matytis:  useris, suma, produktai, data.
-
+// !----------------------------------------------------------------!
+// 8. TODO: <-- BONUS TASK --> Pasidaryti api wrapperi
 export const Cart = () => {
   const cart = useSelector(state => state.cart)
   const isSomeItemsInCart = !!cart.length
